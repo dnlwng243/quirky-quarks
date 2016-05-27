19d8ae6d97024dbafe4e100aad65f71e59a741bd
Daniel Wang <danielwang243@gmail.com>
2016-05-27 12:46:56 -0700

    no title bar

--- app/src/main/java/org/beautiful_butterflies/quirky_quarks/Game.java
+++ app/src/main/java/org/beautiful_butterflies/quirky_quarks/Game.java
@@ -2,6 +2,7 @@ package org.beautiful_butterflies.quirky_quarks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Game extends AppCompatActivity {
 
