6256c5d300103bcea52e987a7d33b2db2eb5672e
Daniel Wang <danielwang243@gmail.com>
2016-05-27 12:40:44 -0700

    back button

--- app/src/main/java/org/beautiful_butterflies/quirky_quarks/StandardModel.java
+++ app/src/main/java/org/beautiful_butterflies/quirky_quarks/StandardModel.java
@@ -1,13 +1,27 @@
 package org.beautiful_butterflies.quirky_quarks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StandardModel extends AppCompatActivity {
+
+    Button modelToMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standard_model);
+
+        modelToMain = (Button) findViewById(R.id.modelToMain);
+
+        modelToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StandardModel.this, MainActivity.class));
+            }
+        });
     }
 }
