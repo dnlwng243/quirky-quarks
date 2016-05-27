37a5d38a68ca572f7abb6d92a63159fca3213142
Daniel Wang <danielwang243@gmail.com>
2016-05-27 12:09:14 -0700

    buttons baby

--- app/src/main/java/org/beautiful_butterflies/quirky_quarks/MainActivity.java
+++ app/src/main/java/org/beautiful_butterflies/quirky_quarks/MainActivity.java
@@ -2,13 +2,22 @@ package org.beautiful_butterflies.quirky_quarks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    
+    Button playButton;
+    Button standardModelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playButton = (Button) findViewById(R.id.playButton);
+        standardModelButton = (Button) findViewById(R.id.standardModelButton);
+
+
     }
 }
 
