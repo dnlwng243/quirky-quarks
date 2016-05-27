d855911247dfaa882a5d45993241fbd4039bce60
Daniel Wang <danielwang243@gmail.com>
2016-05-27 13:19:44 -0700

    added standard model

--- app/src/main/java/org/beautiful_butterflies/quirky_quarks/StandardModel.java
+++ app/src/main/java/org/beautiful_butterflies/quirky_quarks/StandardModel.java
@@ -6,15 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class StandardModel extends AppCompatActivity {
 
     Button modelToMain;
+    ImageView standardModelImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standard_model);
+
+        standardModelImage = (ImageView) findViewById(R.id.standardModelImage);
 
     }
 }
