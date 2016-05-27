d9c77ab8a1a545bf813ee38accf52881926c72dd
Daniel Wang <danielwang243@gmail.com>
2016-05-27 12:25:49 -0700

    actual classes and now layouts

--- app/src/main/java/org/beautiful_butterflies/quirky_quarks/GameActivity.java
+++
@@ -1,7 +0,0 @@
-package org.beautiful_butterflies.quirky_quarks;
-
-/**
- * Created by Jerry on 5/27/2016.
- */
-public class GameActivity {
-}
---
+++ app/src/main/java/org/beautiful_butterflies/quirky_quarks/StandardModel.java
@@ -0,0 +1,13 @@
+package org.beautiful_butterflies.quirky_quarks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StandardModel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standard_model);
+    }
+}
